964819f77d97a3508def2595e5536e4188309806
Utkarsh-sriv <utkarshsrisrivastava@gmail.com>
2024-07-20 01:47:11 +0530

    Removing key arg

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -57,7 +57,7 @@ export const Collection = ({
       {images.length > 0 ? (
         <ul className="collection-list">
           {images.map((image) => (
-            <Card image={image} key={image._id} />
+            <Card image={image} />
           ))}
         </ul>
       ) : (
