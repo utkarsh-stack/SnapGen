361df7d991f70a1fcc50132f16ab1c3c949c61e9
Utkarsh-sriv <utkarshsrisrivastava@gmail.com>
2024-07-20 01:55:26 +0530

    Minor fix

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -16,8 +16,10 @@ import { IImage } from "@/lib/database/models/image.model";
 import { formUrlQuery } from "@/lib/utils";
 
 import { Button } from "../ui/button";
-
 import { Search } from "./Search";
+
+
+
 
 
 export const Collection = ({
